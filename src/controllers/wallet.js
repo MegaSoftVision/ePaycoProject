729e147742a280dcb196dd42a0c2a7638b69e90b
MegaSoftVision <45824438+MegaSoftVision@users.noreply.github.com>
2021-02-25 14:31:00 -0500

    Versión inicial del proyecto 2
    Se agrego algunas vistas con respecto a las wallets, su funcionalidad de ser recargada y renderizada

--- src/controllers/wallet.js
+++ src/controllers/wallet.js
@@ -1,5 +1,5 @@
 const Wallet = require('../models/Wallet');
-
+const User = require('../models/User');
 module.exports = {
     index: async (req, res) => {
         await Wallet.find({user: req.user.id}).sort({date: 'desc'})
@@ -19,5 +19,20 @@ module.exports = {
             console.log(context);
             res.render('Wallet/all-wallets', {wallets: context.wallets }) 
         })
-    }
+    },
+    charge: async (req, res, next) => {
+        const id_wallet = req.params.id;
+        const { document, phone, balance} = req.body;
+        if(document ==  req.user.document && phone == req.user.phone){  
+            const wallet = await Wallet.findById(id_wallet);
+            const oldbalance = wallet.balance;
+            const newbalance = Number(balance) + Number(oldbalance);
+            await Wallet.findByIdAndUpdate(id_wallet, {balance: newbalance});
+            req.flash('success_msg', 'Charge Successfully');
+            res.redirect("/wallets");
+        } else {
+            req.flash('error_msg', 'Document or Phone Incorrect');
+            res.redirect('/wallets/charge/' + id_wallet);
+        }
+    }  
 };
--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -1,11 +1,20 @@
 
 const router = require('express-promise-router')();
+const Wallet = require('../models/Wallet');
 const passport = require('passport');
-const {index} = require('../controllers/wallet');
+const {index, charge} = require('../controllers/wallet');
 const { isAuthenticated } = require('../helpers/auth');
 
-router.get('/wallet', isAuthenticated, index);
+router.get('/wallets', isAuthenticated, index);
 
+router.get('/wallets/charge/:id', isAuthenticated, async (req, res) =>{
+    await Wallet.findById(req.params.id)
+    .then(wallet => {
+        res.render('wallet/charge', {wallet})
+    });
+    
+});
+router.put('/wallets/charge/:id', isAuthenticated, charge);
 
 
 module.exports = router;
